bd914b7bb89a453e5070a13800b709506c6b6d82
Zhiger <nukejanjiger81@gmail.com>
2021-03-09 18:44:20 +0600

    array: array roj

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == null || array.length == 0){
+    return 0;
+  }
+  let minimum = Math.min.apply(null, array)
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array == null || array.length == 0){
+    return 0;
+  }
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  
+  let sum = 0;
+  
+    if(array == null || array.length == 0){
+      return 0;
+    }
+    for(let i = 0; i < array.length; i ++){
+          sum = sum + array[i]; 
+          
+      }
+     
+    let s = sum/array.length;
+    return s ;
 }
